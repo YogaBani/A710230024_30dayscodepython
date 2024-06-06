66aaa13dce5451de0a89cb2e7dd91d7f8a187899
YogaBani <162659778+YogaBani@users.noreply.github.com>
2024-06-06 10:15:29 +0700

    Create Day 2.py

---
+++ Day 2.py
@@ -0,0 +1,9 @@
+# Get the two numbers from the user
+num1 = int(input("Masukkan angka pertama: "))
+num2 = int(input("Masukkan angka kedua: "))
+
+# Add the two numbers
+result = num1 + num2
+
+# Print the result
+print("Hasilnya:", result)
