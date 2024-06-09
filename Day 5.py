b7044d8195ad964f6dc9cd4c073771a814b38f77
YogaBani <162659778+YogaBani@users.noreply.github.com>
2024-06-09 09:39:04 +0700

    Day 5.py

---
+++ Day 5.py
@@ -0,0 +1,41 @@
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", num1 + num2)
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", num1 - num2)
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", num1 * num2)
+
+        elif choice == '4':
+            if num2!= 0:
+                print(num1, "/", num2, "=", num1 / num2)
+            else:
+                print("Error! Division by zero is not allowed.")
+        
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    else:
+        print("Invalid Input")
